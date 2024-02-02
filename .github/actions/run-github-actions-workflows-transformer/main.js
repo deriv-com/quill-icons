ee1b8fdb2387653b80af777f2176a73911227b1d
Niloofar <niloofar.sadeghi@regentmarkets.com>
2024-02-02 13:36:03 +0800

    ci: figma workflow

---
+++ .github/actions/run-github-actions-workflows-transformer/main.js
@@ -0,0 +1,19 @@
+const cp = require('child_process');
+cp.execSync(`cd ${__dirname}; npm ci`);
+
+const core = require('@actions/core');
+
+const fileKey = core.getInput('fileKey', { required: true });
+const page = JSON.parse(core.getInput('page', { required: true }) || '{}');
+const selection = JSON.parse(core.getInput('selection', { required: false }) || '[]');
+
+function run() {
+  core.setOutput('fileKey', fileKey);
+  core.setOutput('page', page.name);
+  core.setOutput(
+    'selection',
+    selection.map((sel) => sel.id),
+  );
+}
+
+run();
